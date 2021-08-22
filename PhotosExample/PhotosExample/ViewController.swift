b6a3b9b906c4ed0a27b7dea3e0a60198ce30db7a
서시언 <siun0816@hanyang.ac.kr>
2021-08-22 17:53:40 +0900

    2021/08/22

--- PhotosExample/PhotosExample/ViewController.swift
+++ PhotosExample/PhotosExample/ViewController.swift
@@ -55,7 +55,6 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
         
         let photoAuthorizationStatus = PHPhotoLibrary.authorizationStatus()
         
