12ef5e569bc0331e8e34f75b49741f56e1a7dd23
SeoSiun <siun0816@hanyang.ac.kr>
2021-08-26 00:11:39 +0900

    2021/08/25 - 컬렉션뷰 셀 커스터마이징

---
+++ FriendsCollection/FriendsCollection/FriendCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  FriendCollectionViewCell.swift
+//  FriendsCollection
+//
+//  Created by 서시언 on 2021/08/25.
+//
+
+import UIKit
+
+class FriendCollectionViewCell: UICollectionViewCell {
+    @IBOutlet var nameAgeLabel: UILabel!
+    @IBOutlet var addressLabel: UILabel!
+    
+    
+}
